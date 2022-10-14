01b2d42e496e29964919e32190bad8a2a49cc77f
Henrique <henrique.krastins@gmail.com>
2022-10-14 16:47:14 -0300

    try: heroku

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,19 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { Pool } = pg;
 
-// const connection = {
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// };
-
 const connection = {
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 };
+
+// const connection = {
+//   connectionString: 'postgres://postgres:12345@localhost:5432/shortly',
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// };
 
 const db = new Pool(connection);
 export default db;
