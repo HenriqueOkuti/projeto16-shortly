5a393ce7def39c2c3d4ac589959bcb3331d0ab0c
Henrique <henrique.krastins@gmail.com>
2022-10-14 16:56:24 -0300

    try: heroku

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes/router.js';
 
-dotenv.config();
-const server = express();
-server.use(cors());
-server.use(json());
-server.use(router);
+export default function server() {
+  dotenv.config();
+  const server = express();
+  server.use(cors());
+  server.use(json());
+  server.use(router);
 
-const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+  server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+}
